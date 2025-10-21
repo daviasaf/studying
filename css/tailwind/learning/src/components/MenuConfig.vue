c2929999ef0d584a83ed2d0efc01bc6e02f17e70
Davi Asaf <daviasaf16@gmail.com>
2025-10-21 20:35:13 -0300

    Remoção de arquivo indesejado

---
+++ css/tailwind/learning/src/components/MenuConfig.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <img src="../../public/menu-dots.svg" alt="menu suspenso" class="w-6 h-6 p-1 transition hover:bg-gray-300 rounded">
+</template>
+
+<style scoped></style>
